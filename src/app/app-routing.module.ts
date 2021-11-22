e11fda1eb564d82b9d2c7ff81c7a0589267635da
Carlos Eduardo de Camargo <cadu.camargoads@gmail.com>
2021-11-22 17:25:09 -0300

    Novas alterações e criação de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/template/home/home.component';
+import { ProdutosComponent } from './components/template/produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path:'home',component: HomeComponent},
+  {path:'produtos',component: ProdutosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/template/home/home.component';
+import { ProdutosComponent } from './components/template/produtos/produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
